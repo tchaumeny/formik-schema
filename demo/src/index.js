699bd0865a4bfd12f1bd61dc3359cc4bc09ea504
Thomas Chaumeny <t.chaumeny@gmail.com>
2017-10-21 14:34:00 +0200

    Rearrange demo to make it compatible with codesandbox

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
 
-import { Form } from '../src/index';
+import { Form } from 'formik-schema';
 
 
 const schema1 = {
